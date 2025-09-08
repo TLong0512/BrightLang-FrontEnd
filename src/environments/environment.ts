ad111ccf962dd12de3eca411ed4fa1efe5a42dfe
pmtben <phammanhtoan19@gmail.com>
2025-09-08 09:58:02 +0700

    update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrlPractice: 'http://localhost:5000/api/Practice',
-  apiUrlVocabulary: 'http://localhost:5000/api/Vocabulary',
-  apiUrlBook: 'http://localhost:5000/api/Book',
+  apiUrlPractice: '/Practice',
+  apiUrlVocabulary: '/Vocabulary',
+  apiUrlBook: '/Book',
   apiUrlAuth: 'http://localhost:5000/api',
   apiUrlQuestionBank: 'http://localhost:5000/api',
   apiUrlRoadMap: 'http://localhost:5000/api'
