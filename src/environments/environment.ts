6b3edcc3a1b5d7cdc6a78c2797463661114523eb
phuthanhban1 <phuthanhban3@gmail.com>
2025-09-08 19:15:49 +0700

    recommit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,7 @@ export const environment = {
   apiUrlVocabulary: '/Vocabulary',
   apiUrlBook: '/Book',
   apiUrlAuth: 'http://localhost:5000/api',
-  apiUrlQuestionBank: 'http://localhost:5000/api' ,
-<<<<<<< HEAD
-  apiUrlRoadMap: 'http://localhost:5000/api' ,
-=======
+  apiUrlQuestionBank: 'http://localhost:5000/api',
   apiUrlRoadMap: 'http://localhost:5000/api' 
->>>>>>> 56e4c8e427d48649344c5f2a2cf72a58d9364b80
+
 };
