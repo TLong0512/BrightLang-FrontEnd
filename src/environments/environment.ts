2f29262057f5337e077dd5327b831f6ac86afac6
phuthanhban1 <phuthanhban3@gmail.com>
2025-09-08 09:55:26 +0700

    fix add question

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrlPractice: 'http://localhost:5000/api/Practice',
   apiUrlVocabulary: 'http://localhost:5000/api/Vocabulary',
-  apiUrlBook: 'http://localhost:5000/api/Book'
+  apiUrlBook: 'http://localhost:5000/api/Book',
   apiUrlAuth: 'http://localhost:5000/api',
   apiUrlQuestionBank: 'http://localhost:5000/api' ,
   apiUrlRoadMap: 'http://localhost:5000/api' ,
