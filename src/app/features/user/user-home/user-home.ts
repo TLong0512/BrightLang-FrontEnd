3cf31bee44e09d1cee14506baf50aec003f28cf5
phuthanhban1 <phuthanhban3@gmail.com>
2025-09-10 01:42:00 +0700

    update admin

--- src/app/features/user/user-home/user-home.ts
+++ src/app/features/user/user-home/user-home.ts
@@ -4,10 +4,10 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 @Component({
     selector: 'user-home',
     standalone: true,
-    templateUrl: 'user-home.html',
-    styleUrl: 'user-home.css',
+    templateUrl: './user-home.html',
+    styleUrl: './user-home.css',
     imports: [RouterLinkActive, RouterLink]
 
 })
 
-export class UserHomeComponent{}
+export class UserHomeComponent {}
